45a64beb6489f5920712c213ab49880d7cc10b15
Twullur <189119157+Twullur@users.noreply.github.com>
2025-01-11 09:40:37 +0700

    made the pseudocode in the js file

--- js.js
+++ js.js
@@ -1,1 +1,31 @@
-console.log("Hello")
+
+
+// create empty variables userChoice, computerChoice, userScore, computerScore
+
+// function playRound () = loop 5 times
+    // getuserchoice = Prompt user for rock, paper, or scissors and store as variable 'userChoice'
+    // get compterChoice = generate computer random choice and store as variable 'computerChoice'
+    // switch
+        // user --> rock and computer --> paper OR
+        // user --> paper and computer --> scissors OR
+        // user --> scissors and computer --> rock 
+        // then console log YOU LOSE computerChoice beats userChoice
+        // computer score ++
+
+        // user --> rock and computer --> scissors OR
+        // user --> scissors and computer --> paper OR
+        // user --> paper and computer --> rock 
+        // then console log YOU WIN userChoice beats computerChoice
+        // user score ++
+
+        // user --> rock and computer --> rock OR
+        // user --> paper and compuer --> paper OR 
+        // user --> scissors and computer --> scissors 
+        // then console log TIE
+
+    
+// if userscore > computerscore, YOU WIN
+    // if userScore < computer score, YOU LOSE
+    // else say TIE
+
+// show userscore and computerscore
